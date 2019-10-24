2a141375f791d8519cd0896a3e41b0d0b298f52d
blue <belkihelmehdi2@gmail.com>
2019-10-24 18:24:37 +0100

    feat: add constants

---
+++ constants.js
@@ -0,0 +1,7 @@
+export const velocity = 5;
+export const edge= 40;
+
+export const texture = PIXI.Texture.from('images/background-blue.png');
+export const spaceship = PIXI.Texture.from('images/pixel_ship_blue.png');
+export const enenmy_spaceship = PIXI.Texture.from('images/pixel_ship_green_small.png');
+export const spaceship_laser = PIXI.Texture.from('images/pixel_laser_small_blue.png');
